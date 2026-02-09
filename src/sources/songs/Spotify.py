5f5e738cd7e71a195daf1fc769e086903859b602
mzietlow <zietlow.m@gmail.com>
2026-02-09 17:42:35 +0100

    Fix: Update playlist handling to support multiple playlist IDs and ensure proper initialization

--- src/sources/songs/Spotify.py
+++ src/sources/songs/Spotify.py
@@ -3,6 +3,8 @@ import os
 import spotipy
 from spotipy import SpotifyClientCredentials
 
+from ..utils import list_or_empty
+
 from .SongsSourceBase import SongsSourceBase
 from ..SongSearchItem import SongSearchItem
 
@@ -11,14 +13,14 @@ class Spotify(SongsSourceBase):
 
     CLIENT_ID = ""
     CLIENT_SECRET = ""
-    PLAYLIST_ID = []
+    PLAYLIST_IDS = []
 
     def __init__(self, user_args):
         self.CLIENT_ID = user_args["spotify_id"] or os.getenv("SPOTIPY_CLIENT_ID")
         self.CLIENT_SECRET = user_args["spotify_secret"] or os.getenv("SPOTIPY_CLIENT_SECRET")
-        self.PLAYLIST_ID = user_args["spotify_input"] or os.getenv("SPOTIPY_PLAYLIST_ID")
+        self.PLAYLIST_IDS = user_args["spotify_input"] or list_or_empty(os.getenv("SPOTIPY_PLAYLIST_ID"))
 
-        if self.PLAYLIST_ID and not (self.CLIENT_ID and self.CLIENT_SECRET): self.raise_error(
+        if self.PLAYLIST_IDS and not (self.CLIENT_ID and self.CLIENT_SECRET): self.raise_error(
             "Client ID and secret are required if a Spotify playlist is specified")
 
         super().__init__()
@@ -33,33 +35,34 @@ class Spotify(SongsSourceBase):
             item = SongSearchItem(name_set, artist_set)
             search_list.append(item)
 
-        print(f"Successfully got parsed playlist from Spotify: {self.PLAYLIST_ID}")
+        print(f"Successfully got parsed playlist from Spotify: {self.PLAYLIST_IDS}")
         return search_list
 
 
     def _get_all_tracks(self):
         auth_manager = SpotifyClientCredentials(self.CLIENT_ID, self.CLIENT_SECRET)
         spotify_client = spotipy.Spotify(auth_manager=auth_manager)
-        playlist_identifier = self.PLAYLIST_ID
 
         tracks = []
-        offset = 0
-        limit = 100
+        
+        for playlist_identifier in self.PLAYLIST_IDS:
+            offset = 0
+            limit = 100
 
-        while True:
-            print(f"Query Tracks {offset} to {offset + limit}")
-            playlist = spotify_client.playlist_items(
-                playlist_id=playlist_identifier,
-                fields="items(track(name,artists(name)))",
-                offset=offset,
-                limit=limit
-            )
-            items = playlist["items"]
+            while True:
+                print(f"Query Tracks {offset} to {offset + limit}")
+                playlist = spotify_client.playlist_items(
+                    playlist_id=playlist_identifier,
+                    fields="items(track(name,artists(name)))",
+                    offset=offset,
+                    limit=limit
+                )
+                items = playlist["items"]
 
-            if not items:
-                break
+                if not items:
+                    break
 
-            tracks.extend(items)
-            offset += limit
+                tracks.extend(items)
+                offset += limit
 
         return tracks
